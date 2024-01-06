3234128984f4298e9cbe7c035ea7055bece1004a
Hash Verma <harsh.verma@canopusinfosystems.com>
2024-01-06 16:18:19 +0530

    adding moon venus background with three

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Home from "./components/Home.jsx"
+import Home from "./components/Home/Home.jsx"
 import Header from "./components/Header.jsx"
 
 function App() {
